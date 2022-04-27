450a9437615aefb93e4e362e1a5d967d12bc2c9c
danieldamian09 <humbertorivero9@gmail.com>
2022-04-27 18:37:50 -0300

    funciones retorno y argumentos

--- src/typescript/Funciones.tsx
+++ src/typescript/Funciones.tsx
@@ -1,10 +1,14 @@
-
 const Funciones = () => {
-  return (
-    <>
-      <h3>Funciones</h3>
-    </>
-  )
-}
+  const sumar = (a:number, b:number):number => {
+    return a + b;
+  };
 
-export default Funciones
+	return (
+		<>
+			<h3>Funciones</h3>
+			<span>El resultado de la suma es: {sumar(2, 3)}</span>
+		</>
+	);
+};
+
+export default Funciones;
