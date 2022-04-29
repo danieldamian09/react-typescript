d14ab0a116a7cc47ed77333bb671bb9f8edf7378
danieldamian09 <humbertorivero9@gmail.com>
2022-04-29 13:52:03 -0300

    hook useReducer

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import { useReducer } from 'react'
+
+// Creamos la Interface de mi estado Inicial
+interface AuthSate{
+  validando: boolean,
+  token: string | null,
+  username: string,
+  nombre: string,
+}
+
+
+// Defino mi estado inicial
+const initialState: AuthSate = {
+  validando: true,
+  token: null,
+  username: "",
+  nombre: "",
+}
+
+// Creamos la accion que preferiblemte la manejamos como type (porque son objetos planos y no lo vamos a expandir en el futuro)
+type AuthAction = {type: "logout" }
+
+
+// Creamos la función (reducer) que recibe el estado y la acción(modifica el estado)
+const authReducer = (state: AuthSate, action: AuthAction): AuthSate => {
+
+}
 
 const Login = () => {
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
   return (
     <>
       <h3>Login</h3>
