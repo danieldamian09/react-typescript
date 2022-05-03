4be31b61b5ae85bd381f61daa00037f5be7cf54f
danieldamian09 <humbertorivero9@gmail.com>
2022-05-03 14:46:11 -0300

    show users en table

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,36 +1,43 @@
 import {useEffect, useState} from "react";
-import { reqResApi } from '../api/reqRes';
-import { ReqRespListado, Usuario } from "../interfaces/reqRes";
+import {reqResApi} from "../api/reqRes";
+import {ReqRespListado, Usuario} from "../interfaces/reqRes";
 
 const Usuarios = () => {
+	const [usuarios, setUsuarios] = useState<Usuario[]>([]);
 
-  const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+	useEffect(() => {
+		// llamado API
+		reqResApi
+			.get<ReqRespListado>("/users")
+			.then((res) => {
+				setUsuarios(res.data.data);
+			})
+			.catch((err) => console.log(err));
+	}, []);
 
-
-  useEffect(() => {
-    // llamado API
-    reqResApi.get<ReqRespListado>("/users").then(res => {
-      setUsuarios(res.data.data);
-    }).catch(err => console.log(err));
-  }, [])
-  
-
+	const renderItem = (usuario: Usuario) => {
+		return (
+			<tr key={usuario.id.toString()}>
+				<td><img src={usuario.avatar} alt="avatar" /></td>
+				<td>{usuario.first_name}</td>
+				<td>{usuario.email}</td>
+			</tr>
+		);
+	};
 
 	return (
 		<>
-      <h3>Usuarios</h3>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Avatar</th>
-            <th>Nombre</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-
-        </tbody>
-      </table>
+			<h3>Usuarios</h3>
+			<table className="table">
+				<thead>
+					<tr>
+						<th>Avatar</th>
+						<th>Nombre</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>{usuarios.map((usuario) => renderItem(usuario))}</tbody>
+			</table>
 		</>
 	);
 };
